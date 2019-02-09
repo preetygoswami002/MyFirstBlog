64667cc322bc1e6266d8a0aa6d98e7f8eb325cfc
preetygoswami002@gmail.com <preety@crowdanalytix.com>
2019-02-09 15:34:53 +0530

    added html file

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def post_list(request):
-    return render(request, 'blog/template/blog/post_list.html', {})
+    return render(request, 'template/blog/post_list.html', {})
